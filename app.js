419c0c20b53600a86bf84cda9db027b278b61982
unknown <ddaksh.2711@gmail.com>
2020-07-01 17:36:15 +0530

    add gitignore

--- app.js
+++ app.js
@@ -736,6 +736,11 @@ app.post("/login", function (req, res) {
 });
 
 //------------------------------------------SERVER RUNNING ON PORT 3000----------------------------------------
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Server is up and Running!!!");
 });
